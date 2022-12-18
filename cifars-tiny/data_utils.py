79181cc471fdc0a549088b04e84d3fdf008ba2ea
gsoykan <gsoykan@sabanciuniv.edu>
2022-12-18 13:57:49 +0300

    wip - use cifars for single gpu training comics crops

--- cifars-tiny/data_utils.py
+++ cifars-tiny/data_utils.py
@@ -3,31 +3,44 @@ import torchvision
 import transform_utils as utils
 import random
 
+from custom_datasets.ssl_comics_crops_dataset import SSLComicsCropsDataset
+
 
 def make_data(dataset, subset, subset_type):
     """Build data folder, includes defined image augmentations in utils as function.
     """
     if dataset == 'cifar10':
         pretrain_data = torchvision.datasets.CIFAR10(root="data", train=True, \
-                                                transform=utils.PretrainTransform(dataset), download=True)
+                                                     transform=utils.PretrainTransform(dataset), download=True)
         lin_train_data = torchvision.datasets.CIFAR10(root="data", train=True, \
-                                                transform=utils.EvalTransform(dataset,train_transform = True), download=True)
+                                                      transform=utils.EvalTransform(dataset, train_transform=True),
+                                                      download=True)
 
         lin_test_data = torchvision.datasets.CIFAR10(root="data", train=False, \
-                                                transform=utils.EvalTransform(dataset,train_transform = False), download=True)
+                                                     transform=utils.EvalTransform(dataset, train_transform=False),
+                                                     download=True)
     if dataset == 'cifar100':
         pretrain_data = torchvision.datasets.CIFAR100(root="data", train=True, \
-                                                transform=utils.PretrainTransform(dataset), download=True)
+                                                      transform=utils.PretrainTransform(dataset), download=True)
         lin_train_data = torchvision.datasets.CIFAR100(root="data", train=True, \
-                                                transform=utils.EvalTransform(dataset,train_transform = True), download=True)
+                                                       transform=utils.EvalTransform(dataset, train_transform=True),
+                                                       download=True)
         lin_test_data = torchvision.datasets.CIFAR100(root="data", train=False, \
-                                                transform=utils.EvalTransform(dataset,train_transform = False), download=True)
+                                                      transform=utils.EvalTransform(dataset, train_transform=False),
+                                                      download=True)
     if dataset == 'tiny_imagenet':
-        pretrain_data = torchvision.datasets.ImageFolder(root="data/tiny-imagenet-200/train",  \
-                                                transform=utils.PretrainTransform(dataset))
-        lin_train_data = torchvision.datasets.ImageFolder(root="data/tiny-imagenet-200/train",  \
-                                                transform=utils.EvalTransform(dataset,train_transform = True))
+        pretrain_data = torchvision.datasets.ImageFolder(root="data/tiny-imagenet-200/train", \
+                                                         transform=utils.PretrainTransform(dataset))
+        lin_train_data = torchvision.datasets.ImageFolder(root="data/tiny-imagenet-200/train", \
+                                                          transform=utils.EvalTransform(dataset, train_transform=True))
         lin_test_data = torchvision.datasets.ImageFolder(root="data/tiny-imagenet-200/val", \
-                                                transform=utils.EvalTransform(dataset,train_transform = False))                   
-                                           
-    return  pretrain_data, lin_train_data, lin_test_data 
+                                                         transform=utils.EvalTransform(dataset, train_transform=False))
+    if dataset == 'comics_crops_bodies':
+        dataset = SSLComicsCropsDataset(
+            prefiltered_csv_folder_dir='/scratch/users/gsoykan20/projects/amazing-mysteries-of-gutter-demystified/data/ssl/filtered_all_body.csv',
+            transform=utils.PretrainTransform(dataset),
+            item_type='body'
+        )
+        pretrain_data, lin_train_data, lin_test_data = dataset, None, None
+
+    return pretrain_data, lin_train_data, lin_test_data
